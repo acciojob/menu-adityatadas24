2ca5a0ecf6deaa81e3c9a8ed9434c0aac751e1b7
adityatadas24 <adityatadas2000@gmail.com>
2023-12-20 13:46:57 +0530

    submitting code-Wed Dec 20 2023 13:46:55 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,57 @@
 <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
 
+
+const App = ()=>{
+
+    
+    const [categories, setCategories] = useState(MovieList);
+   
+  
+    function filterItems (){
+  
+       setCategories(MovieList)
+  
+    };
+    function breakfastItem(){
+      setCategories(categories.filter((item) => item.category== "breakfast"));
+    } 
+  
+    function lunchItem (){
+      setCategories(categories.filter((item) => item.category== "lunch"));
+    }
+  
+    function shakeItem(){
+      setCategories(categories.filter((item) => item.category == "shakes"))
+    }
+  
+    return(
+      <div>
+        <nav className="menu">
+          <h1> OUR MENU</h1>
+          <div>
+            <h2 onClick={filterItems}>All</h2>
+            <h2 onClick={breakfastItem}>Breakfast</h2>
+            <h2 onClick={lunchItem} >Lunch </h2>
+            <h2 onClick={shakeItem}>Shakes</h2>
+  
+          </div>
+        </nav>
+           
+           {
+            categories.map(item=>(
+              <div>
+               
+                <h1>{item.title}</h1>
+                <p>{item.category}</p>
+                <p>{item.price}</p>
+                <p>{item.desc}</p>
+              </div>
+            ))
+           }
+      
+      </div>
+    )
+  }
+
+  export default App
